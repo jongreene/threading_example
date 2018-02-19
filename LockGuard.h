552b33502651d670e8927fd5c9ce7a667dd5870d
Jonathon Greene <jongreene@ku.edu>
2018-02-19 15:57:01 -0600

    Implemented custom LockGuard.h

--- LockGuard.h
+++ LockGuard.h
@@ -5,39 +5,22 @@
 #ifndef THREADING_EXAMPLE_LOCK_GUARD_H
 #define THREADING_EXAMPLE_LOCK_GUARD_H
 namespace custom_lock_guard _GLIBCXX_VISIBILITY(default) {
-/// Assume the calling thread has already obtained mutex ownership
-/// and manage it.
-    struct adopt_lock_t {
-    };
-
-/// Tag used to make a scoped lock take ownership of a locked mutex.
-    constexpr adopt_lock_t adopt_lock{};
-
-
-/** @brief A movable scoped lock type.
- *
- * A unique_lock controls mutex ownership within a scope. Ownership of the
- * mutex can be delayed until after construction and can be transferred
- * to another unique_lock by move construction or move assignment. If a
- * mutex lock is owned when the destructor runs ownership will be released.
- */
     template<typename _Mutex>
     class LockGuard {
     public:
-        typedef _Mutex mutex_type;
+        LockGuard(std::mutex &_M_device) : _M_device(_M_device) {
+            _M_device.lock();
+            std::cout << std::endl << "\tThread locked!" << std::endl;
+        }
+        // calling thread owns mutex
 
-        explicit LockGuard(mutex_type &__m) : _M_device(__m) { _M_device.lock(); }
-
-        LockGuard(mutex_type &__m, adopt_lock_t) : _M_device(__m) {} // calling thread owns mutex
-
-        ~LockGuard() { _M_device.unlock(); }
-
-        LockGuard(const LockGuard &) = delete;
-
-        LockGuard &operator=(const LockGuard &) = delete;
+        ~LockGuard() {
+            _M_device.unlock();
+            std::cout << std::endl << "\tThread unlocked!" << std::endl;
+        }
 
     private:
-        mutex_type &_M_device;
+        std::mutex &_M_device;
     };
 
 }
--- main.cpp
+++ main.cpp
@@ -2,25 +2,25 @@
 #include <thread>
 #include <mutex>
 #include <random>
+#include <condition_variable>
 #include "LockGuard.h"
 
-static const int num_threads = 10;
+static const int num_threads = 3;
 static std::mutex barrier;
 using namespace custom_lock_guard;
 
-std::random_device rd;  //Will be used to obtain a seed for the random number engine
-std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-std::uniform_int_distribution<> dis(0, 6);
+std::default_random_engine generator;
+std::uniform_int_distribution<int> distribution(1,5);
 
 
 //This function will be called from a thread
 void call_from_thread(int threadNumber) {
     // blocks all other code execution until current thread is finished
-    LockGuard<std::mutex> block_threads_until_finish_this_job(barrier);
+    LockGuard block_threads_until_finish_this_job(barrier);
 
     // for simplicity a random number is generated for both seconds and nanoseconds
-    std::chrono::seconds sec(dis(gen));
-    std::chrono::nanoseconds nan(dis(gen));
+    std::chrono::seconds sec(distribution(generator));
+    std::chrono::nanoseconds nan(0);
 
     std::this_thread::__sleep_for(sec,nan);
     std::cout << "Thread: " << threadNumber << " slept for " << sec.count() << " seconds and " << nan.count() << " nanoseconds." << std::endl;
